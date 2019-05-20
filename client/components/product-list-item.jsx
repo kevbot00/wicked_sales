13e49c6a43453ee9b1da4bd09eb5d5c9b8a8482b
kevin yang <kyang812@yahoo.com>
2019-05-20 21:32:39 +0000

    updated css resizing image

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,11 +3,8 @@ import React from 'react';
 export default function ProductListItem(props) {
   return (
     <div className='col-md-4 col-sm-6'>
-      <div className='card'>
-        <div className='card-img'>
-          <img className='border-bottom cardImg' src={props.product.image} alt=""/>
-
-        </div>
+      <div className='card mb-2'>
+        <img className='card-img border-bottom cardImg' src={props.product.image} alt=""/>
         <div className='card-body'>
           <h5 className='card-title'>{ props.product.name }</h5>
           <p className='card-text'>${ (props.product.price / 100).toFixed(2)}</p>
