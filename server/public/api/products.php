be84970fbead15802063d91ddf6eb5793b03a476
kevin yang <kyang812@yahoo.com>
2019-05-21 23:02:16 +0000

    added status code

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -11,7 +11,8 @@
   require_once 'functions.php';
 
   set_exception_handler( "error_handler" );
-
+  doStuff();
+  
   $output = file_get_contents( 'dummy-products-list.json');
   
   print($output);
