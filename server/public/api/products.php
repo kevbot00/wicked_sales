26bbe4eff0c8837d5262674aad75a0d4bb9379cb
kevin yang <kyang812@yahoo.com>
2019-05-21 23:52:38 +0000

    connection file

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -9,13 +9,21 @@
 // }
 
   require_once 'functions.php';
+  require_once 'db_connection.php';
 
   set_exception_handler( "error_handler" );
-  doStuff();
   
+  if (!$conn){
+    http_response_code(400);
+    die('Connection error: ' . mysqli_connect_error());
+  }
   $output = file_get_contents( 'dummy-products-list.json');
   
   print($output);
+
+  print_r($conn);
+
+  mysqli_close($conn);
 
 
 
