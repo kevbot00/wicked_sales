da08735b0b1182846d3c93c72f25457f7c861829
kevin yang <kyang812@yahoo.com>
2019-05-22 18:41:55 +0000

    added whereClause

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -17,22 +17,36 @@
     throw new Exception('Error' . mysqli_connect_error($conn));
   }
 
-
-  if ( is_numeric( $_GET['id'] ) ){
+  if ( $_GET['id'] ){
+    if ( !is_numeric( $_GET['id'])){
+      throw new Exception('id needs to be a number' . $row_num);
+    }
     $whereClause = "WHERE `id` = {$_GET['id']}";
   } else {
     $whereClause = '';
-    throw new Exception('id needs to be a number');
   }
+
+  // if ( $_GET['id'] > $row ){
+  //   throw new Exception("invalid ID: {$_GET['id']}");
+  // }
+
   $query = "SELECT * FROM `products` $whereClause";
   $result = mysqli_query( $conn, $query );
-  if ( !$result ){
+  if ( $result ){
+    $row_num = mysqli_num_rows( $result );
+    if ( $_GET['id'] > $row_num ){
+      throw new Exception("invalid ID: $row_num");
+    }
+  } else {
     throw new Exception('Error' . mysqli_error( $conn ));
   }
+
   $output = [];
+
   while ( $row = mysqli_fetch_assoc( $result )){
     array_push( $output, $row );
   }
+  
   $json_output = json_encode( $output );
   print( $json_output );
 
